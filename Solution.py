3348c7f7123aadb9ed72b229891381543f29ce89
Milind Prajapat <milindprajapat@gmail.com>
2021-03-29 15:56:39 +0530

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -3,5 +3,5 @@ from Pixelate import Pixelate
 
 if __name__ == "__main__":
     Pixelate(6, 6, "pix_sample_arena-v0", 16, 107)
-    Pixelate.Follow_Path(Pixelate.Path(Pixelate.start, Pixelate.info_dict["Pink"][0]))
-    Pixelate.Follow_Path(Pixelate.Path(Pixelate.info_dict["Pink"][0], Pixelate.info_dict[Pixelate.info_dict["Reveal"][0]]))
+    Pixelate.Follow_Path(Pixelate.Path(Pixelate.start, Pixelate.info_dict["Pink"][0])[:-1])
+    Pixelate.Follow_Path(Pixelate.Path(Pixelate.info_dict["Pink"][0], Pixelate.info_dict[Pixelate.info_dict["Reveal"][0]])[:-1])

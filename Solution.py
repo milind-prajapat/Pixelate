cd012b03d07d03a8f5f1c8efef07dcbb26fa791c
Milind Prajapat <milindprajapat@gmail.com>
2021-04-01 23:28:10 +0530

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -8,13 +8,17 @@ if __name__ == "__main__":
 
     while Pixelate.info_dict["Pink"].shape[0] != 0:
 
-        Pixelate.Follow_Path(Pixelate.Path(current_position, Pixelate.info_dict["Pink"][0])[:-1])
+        Pixelate.Follow_Path(Pixelate.Path(current_position, Pixelate.info_dict["Pink"][0])[1:-1])
         print(colored("Picked", "grey", "on_cyan"))
+
+        Pixelate.Follow_Path(Pixelate.info_dict["Pink"][0].reshape(1,2))
 
         current_position = Pixelate.info_dict[Pixelate.info_dict["Reveal"][0]]
 
-        Pixelate.Follow_Path(Pixelate.Path(Pixelate.info_dict["Pink"][0], Pixelate.info_dict[Pixelate.info_dict["Reveal"][0]])[:-1])
+        Pixelate.Follow_Path(Pixelate.Path(Pixelate.info_dict["Pink"][0], Pixelate.info_dict[Pixelate.info_dict["Reveal"][0]])[1:-1])
         print(colored("Dropped", "grey", "on_cyan"))
+
+        Pixelate.Follow_Path(current_position.reshape(1,2))
 
         print("\n")
 

50100fc6945492b69ba66aa32a0fa601ff5b262a
Milind Prajapat <milindprajapat@gmail.com>
2021-04-03 18:21:38 +0530

    Update for Final Submission

--- Pixelate.py
+++ Pixelate.py
@@ -1,18 +1,18 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-import pretty_errors
-pretty_errors.configure(separator_character = '-',
-                        filename_display    = pretty_errors.FILENAME_EXTENDED,
-                        line_number_first   = True,
-                        display_link        = True,
-                        lines_before        = 5,
-                        lines_after         = 2,
-                        line_color          = pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
-                        code_color          = '  ' + pretty_errors.default_config.line_color,
-                        truncate_code       = True,
-                        display_locals      = True)
-pretty_errors.blacklist('c:/python')
+# import pretty_errors
+# pretty_errors.configure(separator_character = '-',
+#                         filename_display    = pretty_errors.FILENAME_EXTENDED,
+#                         line_number_first   = True,
+#                         display_link        = True,
+#                         lines_before        = 5,
+#                         lines_after         = 2,
+#                         line_color          = pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
+#                         code_color          = '  ' + pretty_errors.default_config.line_color,
+#                         truncate_code       = True,
+#                         display_locals      = True)
+# pretty_errors.blacklist('c:/python')
 
 import cv2
 import gym
@@ -20,7 +20,7 @@ import math
 import numpy as np
 import pybullet as p
 import pix_main_arena
-import pix_sample_arena
+# import pix_sample_arena
 
 from os import system
 from cv2 import aruco
